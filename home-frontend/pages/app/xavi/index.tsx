9ea49da66ba4b00c5481b78fd9319c11b00f21b1
Sanchay Javeria <sanchay.javeria@gmail.com>
2021-11-14 18:05:34 -0800

    add vercel deployment hook

--- home-frontend/pages/app/xavi/index.tsx
+++ home-frontend/pages/app/xavi/index.tsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
     return (
         <p>
-            Welcome to Xavi – making workplace more connected, one Loom at a time!
+            Welcome to Xavi–making workplace more connected, one Loom at a time!
         </p>
     )
 }
