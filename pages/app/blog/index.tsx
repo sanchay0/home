a9159781f63e5d4ec3df2f78e4888c9a41bb8a9d
Sanchay Javeria <sanchay.javeria@gmail.com>
2023-05-27 03:12:13 -0700

    Add firebase skeleton

--- pages/app/blog/index.tsx
+++ pages/app/blog/index.tsx
@@ -1,7 +1,35 @@
+import { useEffect, useState } from 'react'
+import { db } from '../../../firebase/clientApp'
+import { collection, getDocs } from 'firebase/firestore'
+
 export default function HomePage() {
+    const [data, setData] = useState(null)
+
+    useEffect(() => {
+        const blogs = async () => {
+            const response  = await getDocs(collection(db, "blogs"))
+            setData(response.docs)
+        }
+
+        blogs()
+    }, [])
+
     return (
-        <p>
-            Welcome to my blog!
-        </p>
+        <div>
+            <p>
+                Welcome to my blog!
+            </p>
+            {
+                data ? (
+                    <ul>
+                        {
+                            data.map(blog => ({id: blog.id, ...blog.data()})).map(entry => (
+                                <li key={entry.id}>{entry.id}, {entry.title}</li>
+                            ))
+                        }
+                    </ul>
+                ) : <p>Loading...</p>
+            }
+        </div>
     )
 }
