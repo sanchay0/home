c7e2e5421c786167bfe4aa8a63b5fb8a29e785e9
Sanchay Javeria <sanchay.javeria@gmail.com>
2023-06-28 15:35:37 -0700

    Minor

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 
 export default function Custom404() {
-    return <div className="text-black text-center">
+    return <div className="text-center">
         <Image alt="image" width={300} height={300} src="/404.svg" draggable="false" className="select-none" />
         <div className="mt-16">404 | This page could not be found.</div>
     </div>
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,8 +1,10 @@
 const HomePage = () => {
 
     return (
-        <div>
+        <div className="mx-auto max-w-xl">
+            <div className="font-light text-sm mt-16">
             Feel free to shoot me a message on <a className="text-black duration-200 hover:no-underline underline after:content-['_↗']" href="https://twitter.com/sanchayjaveria" title="Twitter" target="_blank" rel="noopener noreferrer">Twitter</a>, or you can also send me an email at <a className="text-black duration-200 hover:no-underline underline after:content-['_↗']" href="mailto:sanchayjaveria@gmail.com" title="Email">sanchayjaveria [at] gmail [dot] com</a>
+            </div>
         </div>
     )
 }
