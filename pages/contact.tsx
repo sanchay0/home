9d54652215b616ab6d66153b55a7595c0bfa60b5
Sanchay Javeria <sanchay.javeria@gmail.com>
2023-07-19 23:24:47 -0700

    Add custom email alias in contact form

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -11,7 +11,7 @@ export default function Contact() {
             <p className="dark:text-white text-black">Contact</p>
                 <div className="grid gap-6 mt-3">
                     <div className="grid grid-cols-1 items-start md:grid-cols-1 text-neutral-500">
-                        <p>Feel free to shoot me a message on <a className="text-black duration-200 hover:no-underline underline after:content-['_↗']" href="https://twitter.com/sanchayjaveria" title="Twitter" target="_blank" rel="noopener noreferrer">Twitter</a>, or you can also send me an email at <a className="text-black duration-200 hover:no-underline underline after:content-['_↗']" href="mailto:sanchayjaveria@gmail.com" title="Email">sanchayjaveria [at] gmail [dot] com</a></p>
+                        <p>Feel free to shoot me a message on <a className="text-black duration-200 hover:no-underline underline after:content-['_↗']" href="https://twitter.com/sanchayjaveria" title="Twitter" target="_blank" rel="noopener noreferrer">Twitter</a>, or you can also send me an email at <a className="text-black duration-200 hover:no-underline underline after:content-['_↗']" href="mailto:hello@sanchayjaveria.com" title="Email">hello@sanchayjaveria.com</a></p>
                     </div>
                 </div>
             </div>
