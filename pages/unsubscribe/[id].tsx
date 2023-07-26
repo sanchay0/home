68934ff6b10e3800e7171b846f4e81b76d72a2b0
Sanchay Javeria <sanchay.javeria@gmail.com>
2023-07-26 00:51:59 -0700

    Make subscriber emails pretty and add ability to unsubscribe

--- pages/unsubscribe/[id].tsx
+++ pages/unsubscribe/[id].tsx
@@ -1,15 +1,20 @@
 import { useRouter } from 'next/router'
+import { useState } from 'react'
 import { deleteSubscriber } from '../../utils/api'
 
 export default function Unsubscribe() {
     const router = useRouter()
     const { id } = router.query
+    const [unsubscribed, setUnsubscribed] = useState(false)
 
     const unsubscriber = async () => {
-        deleteSubscriber(`${id}`)
+        await deleteSubscriber(`${id}`)
+        setUnsubscribed(true)
     }
 
     return (
+        unsubscribed ? 
+        <div>Unsubscribed successfully!</div> :
         <div>
             Sorry to see you go. Please <button
                 type="button"
