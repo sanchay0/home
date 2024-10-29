b58d86ffb2158b49839e370f2b3eacde1fe92803
Sanchay Javeria <sanchay.javeria@gmail.com>
2024-10-29 02:14:56 -0700

    Update api.tsx
    Remove any character that is not a word character (\w), whitespace (\s), or a hyphen (-), and also removes underscores (_) from the title of a newly created blog. This will give URL friendly titles.

--- utils/api.tsx
+++ utils/api.tsx
@@ -124,7 +124,7 @@ export async function putBlog(
   content: string,
   tagNames: string[],
 ): Promise<string> {
-  const postId = title.replace(/\s+/g, "-").toLowerCase();
+  const postId = title.replace(/[^\w\s-]|_/g, "").replace(/\s+/g, "-").toLowerCase();
   const postDocRef = doc(collection(db, "blogs"), postId);
   await setDoc(postDocRef, {
     title,
