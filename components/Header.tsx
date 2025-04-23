cf571b7466dc2aa383fefd3f5e88b1a9d7943acb
Sanchay Javeria <sanchay.javeria@gmail.com>
2025-04-23 13:35:15 -0700

    Remove manual margins from nav bar and use flex properties

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,12 +8,12 @@ export default function Header({ links }: HeaderProps) {
     <div className="hidden lg:grid grid-cols-3 items-center">
       <div className="col-span-1" />
       <div className="col-span-1 flex justify-center">
-        <ul className="flex list-none">
+        <ul className="flex gap-x-8 list-none">
           {links &&
             links.map((link) => (
               <li
                 key={link.href}
-                className="mr-6 hover:text-black transition-colors duration-300"
+                className="hover:text-black transition-colors duration-300"
               >
                 <Link href={link.href}>{link.label}</Link>
               </li>
@@ -22,18 +22,18 @@ export default function Header({ links }: HeaderProps) {
       </div>
       <div className="col-span-1 flex justify-center">
         {currentUser && (
-          <>
+          <div className="flex items-center gap-x-2">
             <span className="capitalize">
               Welcome, {currentUser.displayName.split(" ")[0]}!
             </span>
             <button
               type="button"
-              className="ml-2 flex font-light text-black items-center text-sm duration-200 hover:no-underline underline"
+              className="flex font-light text-black items-center text-sm duration-200 hover:no-underline underline"
               onClick={logout}
             >
               (logout)
             </button>
-          </>
+          </div>
         )}
       </div>
     </div>
