d087ca0514b9296638eeebb6ab38372b54512a8b
Sanchay Javeria <sanchay.javeria@gmail.com>
2025-04-23 14:59:01 -0700

    Update index.tsx

--- pages/quotes/index.tsx
+++ pages/quotes/index.tsx
@@ -12,7 +12,9 @@ export default function Quotes() {
           <div className="grid gap-6 mt-3">
             <div className="grid grid-cols-1 items-start md:grid-cols-1 text-neutral-500">
               <p>
-                I love quotes, the best ones pack a book&apos;s worth of insight into just a few lines. Here are some of my favorites that I like to revisit from time to time.
+                I love quotes, the best ones pack a book&apos;s worth of insight
+                into just a few lines. Here are some of my favorites that I like
+                to revisit from time to time.
               </p>
             </div>
           </div>
